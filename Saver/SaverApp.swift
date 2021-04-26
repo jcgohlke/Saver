df02715a97baa323d06b11cd89f1aab19e0e99a4
Ben Gohlke <jcgohlke@me.com>
2021-04-25 21:11:28 -0400

    Initial Commit

---
+++ Saver/SaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaverApp.swift
+//  Saver
+//
+//  Created by Joben Gohlke on 4/25/21.
+//
+
+import SwiftUI
+
+@main
+struct SaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
